8b045c7e25163e63f52c4ab64daf2486a7e24a76
JCMZZ <jiangchunmingzz@gmail.com>
2019-08-14 18:55:10 +0800

    Element UI selectForm

---
+++ selectFormElementUI/data.js
@@ -0,0 +1,58 @@
+var graphShowMode = [{
+    id: 1,  
+    value: 'lines',
+    label: 'Lines'
+}, {
+    id: 2,
+    value: 'bars',
+    label: 'Bars'
+}, {
+    id: 3,
+    value: 'points',
+    label: 'Points'
+}, {
+    id: 4,
+    value: 'stack',
+    label: 'Stack'
+}, {
+    id: 8,
+    value: 'yaxis',
+    label: 'Y-axis',
+    children: [{
+        id: '1',
+        label: 'LeftY'
+    }, {
+        id: '2',
+        label: 'RightY'
+    }]
+},
+{
+    id: 5,
+    value: 'linewidth',
+    label: 'Line Width',
+    children: [{
+        id: '2',
+        label: '2'
+    }, {
+        id: '4',
+        label: '4'
+    }, {
+        id: '6',
+        label: '6'
+    }]
+}, {
+    id: 7,
+    value: 'linefill',
+    label: 'Line Fill',
+    children: [{
+        id: '2',
+        label: '2'
+    }, {
+        id: '4',
+        label: '4'
+    }, {
+        id: '6',
+        label: '6'
+    }]
+}
+]
