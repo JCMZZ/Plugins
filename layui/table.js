e09d93238e0e302837d82a1f7b20b6551eb50cf9
JCMZZ <jiangchunmingzz@gmail.com>
2019-08-14 15:01:26 +0800

    layui custom tree

---
+++ layui/table.js
@@ -0,0 +1,170 @@
+var table = {
+    "0": {
+        "dutydate": "2019-07-31",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "1": {
+        "dutydate": "2019-07-01",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "2": {
+        "dutydate": "2019-07-02",
+        "dutydirector": "李四",
+        "dutyuser": "张三,李四"
+    },
+    "3": {
+        "dutydate": "2019-07-03",
+        "dutydirector": "张三,李四",
+        "dutyuser": "张三"
+    },
+    "4": {
+        "dutydate": "2019-07-04",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "5": {
+        "dutydate": "2019-07-05",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "6": {
+        "dutydate": "2019-07-06",
+        "dutydirector": "张三",
+        "dutyuser": "张三"
+    },
+    "7": {
+        "dutydate": "2019-07-07",
+        "dutydirector": "李四",
+        "dutyuser": "张三,李四"
+    },
+    "8": {
+        "dutydate": "2019-07-08",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "9": {
+        "dutydate": "2019-07-09",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "10": {
+        "dutydate": "2019-07-10",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "11": {
+        "dutydate": "2019-07-11",
+        "dutydirector": "张三",
+        "dutyuser": "张三"
+    },
+    "12": {
+        "dutydate": "2019-07-12",
+        "dutydirector": "张三",
+        "dutyuser": "张三"
+    },
+    "13": {
+        "dutydate": "2019-07-13",
+        "dutydirector": "张三",
+        "dutyuser": "李四"
+    },
+    "14": {
+        "dutydate": "2019-07-14",
+        "dutydirector": "张三",
+        "dutyuser": "李四"
+    },
+    "15": {
+        "dutydate": "2019-07-15",
+        "dutydirector": "李四",
+        "dutyuser": "张三"
+    },
+    "16": {
+        "dutydate": "2019-07-16",
+        "dutydirector": "张三",
+        "dutyuser": "李四"
+    },
+    "17": {
+        "dutydate": "2019-07-17",
+        "dutydirector": "张三",
+        "dutyuser": "张三"
+    },
+    "18": {
+        "dutydate": "2019-07-18",
+        "dutydirector": "张三",
+        "dutyuser": "李四"
+    },
+    "19": {
+        "dutydate": "2019-07-19",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "20": {
+        "dutydate": "2019-07-20",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "21": {
+        "dutydate": "2019-07-21",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "22": {
+        "dutydate": "2019-07-22",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "23": {
+        "dutydate": "2019-07-23",
+        "dutydirector": "李四",
+        "dutyuser": "张三,李四"
+    },
+    "24": {
+        "dutydate": "2019-07-24",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "25": {
+        "dutydate": "2019-07-25",
+        "dutydirector": "张三",
+        "dutyuser": "李四"
+    },
+    "26": {
+        "dutydate": "2019-07-26",
+        "dutydirector": "张三",
+        "dutyuser": "张三,李四"
+    },
+    "27": {
+        "dutydate": "2019-07-27",
+        "dutydirector": "张三",
+        "dutyuser": "张三"
+    },
+    "28": {
+        "dutydate": "2019-07-28",
+        "dutydirector": "李四",
+        "dutyuser": "张三"
+    },
+    "29": {
+        "dutydate": "2019-07-29",
+        "dutydirector": "张三",
+        "dutyuser": "李四"
+    },
+    "30": {
+        "dutydate": "2019-07-30",
+        "dutydirector": "张三",
+        "dutyuser": "张三"
+    },
+    length: 32
+};
+table = Array.prototype.slice.call(table);
+var times = {
+	"sendtype1": "2",
+	"senddate1": "2",
+	"sendtime1": "00:00:00",
+	"sendtype2": "1",
+	"senddate2": "3",
+	"sendtime2": "00:00:00",
+	"sendtype3": "1",
+	"senddate3": "2",
+	"sendtime3": "00:00:00"
+};
